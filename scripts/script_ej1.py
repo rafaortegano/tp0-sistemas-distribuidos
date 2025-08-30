b5cef1a129e8a8db94f3db61036ec07f719fc693
rafaortegano <rortegano@fi.uba.ar>
2025-08-30 16:57:09 -0300

    remove: LOGGIN_LEVEL from script1_ej1.py since will not be used in the docker-compose

--- scripts/script_ej1.py
+++ scripts/script_ej1.py
@@ -16,7 +16,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
@@ -31,7 +30,6 @@ services:
     entrypoint: /client
     environment:
       - CLI_ID={i}
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     depends_on:
