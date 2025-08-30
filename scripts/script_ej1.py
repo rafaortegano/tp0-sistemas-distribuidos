e33d34b7435c0fbaa09570daa82bc99338e00e92
rafaortegano <rortegano@fi.uba.ar>
2025-08-30 15:04:18 -0300

    add: scripts/script_ej1.py

---
+++ scripts/script_ej1.py
@@ -0,0 +1,47 @@
+import sys 
+
+if len(sys.argv) != 3:
+    print("Número de argumentos inválido. Se esperan 2: archivo de salida y cantidad de cliente")
+    sys.exit(1)
+
+output_file = sys.argv[1]
+clients_number = int(sys.argv[2])
+
+with open(output_file, 'w') as f:
+    f.write("""name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+""")
+
+    for i in range(1, clients_number + 1):
+        f.write(f"""
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+""")
+
+    f.write("""
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+""")
+
