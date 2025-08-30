b8fbb48b36a2a3453e28936b2dc14c9610e89048
rafaortegano <rortegano@fi.uba.ar>
2025-08-30 19:21:57 -0300

    add: validar-echo-server.sh

---
+++ validar-echo-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MESSAGE="Vamos River"
+SERVICE="server"   
+PORT=12345         
+NETWORK="tp0_testing_net" 
+MAX_SECONDS_TO_WAIT=3
+
+
+RESPONSE=$(docker run --rm --network $NETWORK alpine sh -c \
+  "apk add --no-cache netcat-openbsd >/dev/null && echo $MESSAGE | nc -w $MAX_SECONDS_TO_WAIT $SERVICE $PORT")
+
+
+if [ "$RESPONSE" = "$MESSAGE" ]; then
+  echo "action: test_echo_server | result: success"
+  
+else
+  echo "action: test_echo_server | result: fail"
+fi
