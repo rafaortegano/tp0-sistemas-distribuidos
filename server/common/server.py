9de256ff86fb914db8d4e11b6aca156099d65cf6
rafaortegano <rortegano@fi.uba.ar>
2025-09-02 22:21:30 -0300

    fix: result type in log to test

--- server/common/server.py
+++ server/common/server.py
@@ -115,7 +115,7 @@ class Server:
                     raise ValueError(f"Unknown message type: {msg_type}")
                     
         except OSError as e:
-            logging.info(f"action: handle_client | result: finish | error: {e}")
+            logging.error(f"action: handle_client | result: success | error: {e}")
             try:
                 send_bet_response(client_sock, STATUS_ERROR, "Error al procesar mensaje")
             except:
